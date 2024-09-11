97bf14f6c38b386ffed0d3429e954b078b55a29f
DESKTOP-PO4U83T\sushi <sunil.ladhyal.sharma@gmail.com>
2024-09-11 19:35:04 +0530

    React : How to use props in react class based component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
 import { Component } from 'react';
+import Child from './Child';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      firstname : 'sun',
+    _fullName = {
+      fristname:'sun',
       lastname : 'sha'
-    }    
+    }
   }
 
   render() {
-    return <div>
-          <h1>Full Name : {this.state.firstname} {this.state.lastname}</h1>
-          <button onClick={() => {this.setState({firstname : 'sam'})}}> update</button>
-      </div>      
+    return <Child 
+               fullName={firstname: 'sun', lastname: 'sha'}
+               age={28}
+            /> 
   }
 }
-
-
-
 
 export default App;

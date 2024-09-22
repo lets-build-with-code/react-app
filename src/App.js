31334a36bfa1b451d4a1a5416054a611a43879f0
DESKTOP-PO4U83T\sushi <sunil.ladhyal.sharma@gmail.com>
2024-09-22 16:48:09 +0530

    React : Class based component (how to use state in calss based component)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import "./App.css";
 import { Component } from "react";
-import Child from "./Child";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: null,
-    };
+      firstname : 'sun',
+      lastName : 'sha'
+    }
+    // this.firstname = "sun";
+    // this.lastname = "sha";
+    // this.updateValues = this.updateValues.bind(this);
   }
-
-  onReceiveData = (data) => {
-    this.setState({ data });
-  };
-
-  items = (users) => {
-    return users?.map(usr => <li key={usr.id}>{usr.name} - {usr.type}</li>)
-  };
-
 
   render() {
-    
-    return (
-      <div className="container">
-        <div className="container">
-          <p>Parent</p>
-          
-          {this.items(this.state.data)}
-        </div>
-        <Child onReceive={this.onReceiveData} />
-      </div>
-    );
+    return <div>
+       <h1>{this.state.firstname} - {this.state.lastName} </h1>
+       <button onClick={ () => { this.setState({firstname : 'sam'})} }>Update</button>
+    </div>
   }
+    // get firstName() {
+  //   return this.firstname
+  // }
+  // set _firstName(value) {
+  //   this.firstname = value
+  // }
+  // get lastName() {
+  //   return this.lastname
+  // }
+  // set _lastName(value) {
+  //   this.lastname = value
+  // }
+
+  // updateValues = () => {
+  //   console.log({first : this.firstName, last : this.lastName});
+  //   this.forceUpdate();
+  //   this._firstName = 'sam'; this._lastName = 'ver' 
+  // }
 }
 
 export default App;

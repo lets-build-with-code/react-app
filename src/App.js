c5dc5d8ef198b73c9df3b084410eea22ff1ebb3b
DESKTOP-PO4U83T\sushi <sunil.ladhyal.sharma@gmail.com>
2024-09-29 16:17:20 +0530

    React : Hooks (useEffect - mounting & umounting)

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import "./App.css";
 import Child from "./Child";
 
 function App() {
-  const [users, setUsers] = useState([])
-  const getDataFromChild = (data) => {
-    console.log(data);
-    setUsers(data);
-  }
+  const [show , setShow] = useState(true);
   return (
     <div>
-      <div className="container">
-        <h3>Parent Component</h3>
-        {users?.map((usr) => <li key={usr.id}>{usr.name} - {usr.department}</li>)}
-      </div>
-      <Child onReceive={getDataFromChild}/>
-    </div>
+      <h1>Parent Component</h1>
+      { show && <Child /> }
+      <button onClick={() => setShow(!show)}>{ show ? 'HIde' : 'Show'}</button>
+    </div> 
   );
 }
-
-
-
-
 export default App;
--- src/Child.js
+++ src/Child.js
@@ -1,23 +1,17 @@
+import { useEffect } from "react";
+
 function Child(props) {
-  const transferData = () => {
-    const users = [
-      {
-        id: 1,
-        name: "sun",
-        department: "IT",
-      },
-      {
-        id: 2,
-        name: "Adam",
-        department: "Admin",
-      },
-    ];
-    props.onReceive(users);
-  };
+
+  useEffect(() => {
+    console.log({fact : 'Component mount'});
+    return () => {
+      console.log({fact : 'Component will unmount.'})
+    }
+  }, []);
+  
   return (
     <div className="container">
       <h1>Child Component</h1>
-      <button onClick={transferData}>Transfer Data to Parent</button>
     </div>
   );
 }
